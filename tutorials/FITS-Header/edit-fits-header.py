39159b4730913e1b76984a560ac0237d9412d442
Adrian Price-Whelan <adrian.prw@gmail.com>
2013-10-08 13:38:50 -0400

    remove old files

--- tutorials/FITS-Header/edit-fits-header.py
+++
@@ -1,7 +0,0 @@
-import astropy.io.fits as fits
-data, hdr = fits.getdata("inputfile.fits",header=True)
-hdr['OBJECT'] = "M31"
-fits.writeto('outputfile.fits',data,hdr)
-
-data,hdr = fits.getdata("inputfile.fits",ext=2,header=True)
-fits.writeto('outputfile.fits',data,hdr,clobber=True)
