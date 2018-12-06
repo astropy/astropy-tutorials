2750abaada505d88489bd3cc74eeecd4ec54ca34
Stuart Mumford <stuart@cadair.com>
2018-12-06 14:36:39 -0700

    first version of gh-pages deploy script

---
+++ .circleci/gh-pages-push.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -eux
+
+# if [ -z "${GIT_USER_NAME}" ]; then
+#   echo "Please set an env var GIT_USER_NAME"
+#   exit 1
+# fi
+#
+# if [ -z "${GIT_USER_EMAIL}" ]; then
+#   echo "Please set an env var GIT_USER_EMAIL"
+#   exit 1
+# fi
+
+if [[ -z $CIRCLE_PULL_REQUEST ]] ; then
+    git -c user.name='circle' -c user.email='circle' commit -m "now with RST"
+    git clone --single-branch -b gh-pages https://github.com/Cadair/astropy-tutorials gh-pages
+    cp -r _build/html/* gh-pages
+    cd gh-pages
+    git push -q origin gh-pages
+    echo "Not a pull request: pushing RST files to rst branch."
+else
+    echo $CIRCLE_PULL_REQUEST
+    echo "This is a pull request: not pushing RST files."
+fi
