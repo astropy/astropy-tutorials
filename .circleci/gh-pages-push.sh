8a56490b77060fa5755f34cfdbbe7963d7c3fde0
Adrian Price-Whelan <adrian.prw@gmail.com>
2018-12-09 14:58:18 -0500

    nojekyll

--- .circleci/gh-pages-push.sh
+++ .circleci/gh-pages-push.sh
@@ -1,26 +1,18 @@
 #!/bin/bash -eux
 
-# if [ -z "${GIT_USER_NAME}" ]; then
-#   echo "Please set an env var GIT_USER_NAME"
-#   exit 1
-# fi
-#
-# if [ -z "${GIT_USER_EMAIL}" ]; then
-#   echo "Please set an env var GIT_USER_EMAIL"
-#   exit 1
-# fi
-
 if [[ -z $CIRCLE_PULL_REQUEST ]] ; then
     git clone --single-branch -b gh-pages git@github.com:astropy/astropy-tutorials.git gh-pages
     cp -r build/html/* gh-pages
     cd gh-pages
     git add .
+    touch .nojekyll
+    git add .nojekyll
     git status
     git -c user.name='circle' -c user.email='circle' commit -m "Upadate the build docs"
     git status
     git push -q origin gh-pages
-    echo "Not a pull request: pushing RST files to rst branch."
+    echo "Not a pull request: pushing website to gh-pages branch."
 else
     echo $CIRCLE_PULL_REQUEST
-    echo "This is a pull request: not pushing RST files."
+    echo "This is a pull request: **not** pushing to gh-pages."
 fi
