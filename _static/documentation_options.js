54328a5f36e6e8dc7d433a840d0f116f1b0033e4
circle <circle>
2020-02-11 15:37:37 +0000

    Upadate the build docs

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -3,6 +3,7 @@ var DOCUMENTATION_OPTIONS = {
     VERSION: '3.0.dev',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
