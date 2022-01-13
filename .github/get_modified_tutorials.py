a40cabfdff3fe822b6039e9e4261efd274096782
Adrian Price-Whelan <adrian.prw@gmail.com>
2022-01-13 12:07:40 -0500

    combine changetype filters and make diffs the correct ordering

--- .github/get_modified_tutorials.py
+++ .github/get_modified_tutorials.py
@@ -4,18 +4,22 @@ from git import Repo
 def main(repo_path, main_branch, **kw):
     repo = Repo(repo_path)
 
-    # Check committed changes on this branch against the main branch:
-    files_changed = repo.git.diff(
-        f'{str(repo.head.object.hexsha)}..{main_branch}',
-        '--name-only').split("\n")
-    files_changed = set([f for f in files_changed if f.endswith('.ipynb')])
+    # Check committed changes on this branch against the main branch,
+    # modified files in staging area,
+    # unstaged changes
+    diff_lists = [
+        repo.commit(main_branch).diff(repo.head),
+        repo.head.commit.diff(),
+        repo.head.commit.diff(None)
+    ]
 
-    # Now check the staging area and unstaged changes:
-    files_changed = files_changed.union([
-        diff.b_path for diff in repo.index.diff("HEAD") + repo.index.diff(None)
-        if diff.change_type in ['M', 'A', 'R']  # modified, added, renamed
-        and diff.b_path.endswith('.ipynb')
-    ])
+    files_changed = set()
+    for diffs in diff_lists:
+        files_changed = files_changed.union([
+            diff.b_path for diff in diffs
+            if diff.change_type in ['M', 'A', 'R']  # modified, added, renamed
+            and diff.b_path.endswith('.ipynb')
+        ])
 
     if files_changed:
         print(" ".join(files_changed))
