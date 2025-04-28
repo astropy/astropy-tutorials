0f2e65fba980c1f4624e62747742be2a9df4875d
Jeff Jennings <jjennings@flatironinstitute.org>
2025-04-28 16:48:01 -0400

    .yml suffix

--- scripts/populate_yml.py
+++ scripts/populate_yml.py
@@ -3,11 +3,11 @@ import yaml
 with open("AUTHORS.md", "r") as f:
     authors = f.read().replace("\n", " ").strip(" ")
 
-with open("metadata.yaml", "r") as f:
+with open("metadata.yml", "r") as f:
     meta = yaml.load(f, Loader=yaml.SafeLoader)
 
 # update _config with tutorial-specific metadata
-with open("astropy-tutorials/scripts/_config.yaml") as f:
+with open("astropy-tutorials/scripts/_config.yml") as f:
     cfg = yaml.load(f, Loader=yaml.SafeLoader)
 
 cfg["title"] = meta["title"]
@@ -17,8 +17,8 @@ cfg["launch_buttons"]["binderhub_url"] = (
     f"https://mybinder.org/v2/gh/astropy-learn/{meta['source']}/main?urlpath=%2Fdoc%2Ftree%2F{meta['slug']}.ipynb"
 )
 
-# '_config.yaml' will be saved in the target repo, keeping the unmodified template in astropy-tutorials
-with open("_config.yaml", "w") as f:
+# '_config.yml' will be saved in the target repo, keeping the unmodified template in astropy-tutorials
+with open("_config.yml", "w") as f:
     cfg = yaml.dump(cfg, stream=f, default_flow_style=False, sort_keys=False)
 
 # print str for use by execute_convert action
