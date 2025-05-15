c503b7feac4b6593970b831242074379132038fa
Jeff Jennings <jjennings@flatironinstitute.org>
2025-05-15 08:26:47 -0400

    copy build outputs to dir for artifact upload

--- scripts/build_notebook.py
+++ scripts/build_notebook.py
@@ -17,10 +17,23 @@ if book is True:
     # build pdf
     command = "jb build . --config _config.yaml --toc _toc.yml --builder pdfhtml"
     os.system(command)    
+
+    # copy build outputs to 'html' dir
+    command = f"cp -r '_build/html' html"
+    os.system(command)
+    command = f"cp '_build/pdf/book.pdf' html"    
+    os.system(command)
+
 else:
     # build single notebook
     command = f"jb build {slug}.ipynb --config _config.yml"
     os.system(command)
     # build pdf 
     command = f"jb build {slug}.ipynb --config _config.yml --builder pdfhtml"
+    os.system(command)
+
+    # copy build outputs to 'html' dir
+    command = f"cp -r '_build/_page/{slug}/html' html"
+    os.system(command)
+    command = f"cp '_build/_page/{slug}/pdf/{slug}.pdf' html"
     os.system(command)
