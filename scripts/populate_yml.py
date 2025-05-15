894bd599030fe7b0f77af50c2e3720813be1006b
Jeff Jennings <jjennings@flatironinstitute.org>
2025-05-15 07:03:16 -0400

    remove binderhub excess button

--- scripts/populate_yml.py
+++ scripts/populate_yml.py
@@ -6,20 +6,22 @@ with open("AUTHORS.md", "r") as f:
 with open("metadata.yml", "r") as f:
     meta = yaml.load(f, Loader=yaml.SafeLoader)
 
-# update _config with tutorial-specific metadata
+# populate _config with tutorial-specific metadata
 with open("astropy-tutorials/scripts/_config.yml") as f:
     cfg = yaml.load(f, Loader=yaml.SafeLoader)
 
 cfg["title"] = meta["title"]
 cfg["author"] = authors
 cfg["repository"]["url"] = f"https://github.com/astropy-learn/{meta['source']}"
-cfg["launch_buttons"]["binderhub_url"] = (
-    f"https://mybinder.org/v2/gh/astropy-learn/{meta['source']}/main?urlpath=%2Fdoc%2Ftree%2F{meta['slug']}.ipynb"
-)
 
-# '_config.yml' will be saved in the target repo, keeping the unmodified template in astropy-tutorials
+# add a button to launch notebook on Binder
+# cfg["launch_buttons"]["binderhub_url"] = (
+#     f"https://mybinder.org/v2/gh/astropy-learn/{meta['source']}/main?urlpath=%2Fdoc%2Ftree%2F{meta['slug']}.ipynb"
+# )
+
+# save populated '_config.yml' in the target repo
 with open("_config.yml", "w") as f:
     cfg = yaml.dump(cfg, stream=f, default_flow_style=False, sort_keys=False)
 
-# print str for use by subsequent job
+# for use by subsequent github actions job
 print(meta["slug"])
